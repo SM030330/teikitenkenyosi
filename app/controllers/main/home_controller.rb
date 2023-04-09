7b7811c91a1fcc6a9ae9fef86fa3dd1bef28e49b
SM030330 <masamasa030330@gmail.com>
2023-04-09 23:08:19 +0900

    直近の予定表、カレンダーの追加 gemfile simple_calendarの追加 home_controller,index.html.erb 直近の予定表、カレンダーの追加 item.rb validationをcreateアクション限定に変更

--- app/controllers/main/home_controller.rb
+++ app/controllers/main/home_controller.rb
@@ -1,4 +1,8 @@
 class Main::HomeController < ApplicationController
   def index
+    if main_user_signed_in?      
+      @coming_items = current_main_user.items.includes(:inspection).where(doing: false).where("do_day >= ? ", Date.current).order( do_day: :asc) 
+      @user_items = current_main_user.items.includes(:inspection)
+    end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,11 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :do_day, presence: true
   validates :notice_day, presence: true
-  validate :do_day_is_today_or_before_today,
-           :notice_day_is_after_do_day
+  validate :do_day_is_today_or_before_today, on: :create
+  validate :notice_day_is_after_do_day
  
   def do_day_is_today_or_before_today
-    if do_day.present? && ( do_day <= created_at if created_at.nil? || do_day <= Date.current )
+    if do_day.present?  && do_day <= Date.current 
       errors.add(:do_day, "登録日より前には設定できません")
     end
   end
