2724789da5578a12f63a2a3df1d4a6203d66fd9c
SM030330 <masamasa030330@gmail.com>
2023-05-02 07:32:51 +0900

    viewの微修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,13 +10,13 @@ class Item < ApplicationRecord
  
   def do_day_is_today_or_before_today
     if do_day.present?  && do_day <= Date.current 
-      errors.add(:do_day, "登録日より前には設定できません")
+      errors.add(:do_day, "は登録日より前には設定できません")
     end
   end
 
   def notice_day_is_after_do_day
     if do_day.present? && notice_day.present? && notice_day >= do_day
-      errors.add(:notice_day, "作業日より後には設定できません")
+      errors.add(:notice_day, "は実施日より後には設定できません")
     end
   end
 end
