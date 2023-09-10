58d9356d1030ae744ee339022621577cd235d408
SM030330 <masamasa030330@gmail.com>
2023-09-10 13:28:09 +0900

    toppage itemの表示の変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def item_doing?(item)
+    if item.doing == true
+      "作業済み"
+    else
+      "作業前"
+    end
+  end
+
+  def item_doing_return_css_class(item)
+    if item.doing == true
+      "calendar_item_doing"
+    else
+      "calendar_item_undoing"
+    end
+  end
 end
